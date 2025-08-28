c7215f5c293aa1d1706fbf76931a8df688dc9e52
William Kershaw <wkershaw456@gmail.com>
2025-08-28 12:03:48 +0100

    Allow indexing of URL path components

--- Morph/Runtime/NativeTypes/Url.cs
+++ Morph/Runtime/NativeTypes/Url.cs
@@ -65,13 +65,29 @@ internal class Url : MorphClass
 
 		object? IMorphIndexable.Get(Interpreter interpreter, object? key)
 		{
-			if (key is not string keyString)
+			if (key is string keyString)
 			{
-				throw new RuntimeException(null, "Can only index Url with a string");
+				var queryString = HttpUtility.ParseQueryString(_uri.Query);
+				return queryString[keyString];
 			}
 
-			var queryString = HttpUtility.ParseQueryString(_uri.Query);
-			return queryString[keyString];
+			if (int.TryParse(key?.ToString(), out int index))
+			{
+				var segments = _uri.Segments;
+				if (index < 0)
+				{
+					index = segments.Length + index;
+				}
+
+				if (index >= 0 && index < segments.Length)
+				{
+					return segments[index].Trim('/');
+				}
+
+				return null;
+			}
+
+			throw new RuntimeException(null, "Can only index Url with a string or int");
 		}
 		public override string ToString()
 		{
